7f48aabe24ac96cd306816a2ca53af13a5312c91
Chocobog <joshua.hurn@griffithuni.edu.au>
2017-05-16 19:57:33 +1000

    minor Config changes

--- php_post.php
+++ php_post.php
@@ -8,7 +8,7 @@ $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
 $mailer = Swift_Mailer::newInstance($transport);
 
 $message = Swift_Message::newInstance('Test Subject')
-  ->setFrom(array('abc@example.com' => 'ABC'))
+  ->setFrom(array('askSalty1@gmail.com' => 'Ask Salty'))
   ->setTo(array('josh.hurn@hotmail.com'))
   ->setBody('This is a test mail.');
 
